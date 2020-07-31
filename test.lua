32517737239892fc12343f6f4e38b245532f8884
PapaTim68 <tim.papenhausen@gmx.de>
2020-07-31 17:13:29 +0200

    io read and nill chekc

--- test.lua
+++ test.lua
@@ -7,12 +7,15 @@ items = inv.getAllStacks(sides.east)
 
 for item_n = 1, items.count() do
     item = items[item_n]
-    if item != nil do
+    if item == nil do
+        print("--EMPTY ".. item_n .. " SLOT--")
+    else
         print("--ITEM " .. item_n .. " START--")
         for k,v in pairs(item) do
             print(k,v)
         end
         print("--ITEM " .. item_n .." END--") 
+        io.read()
     end
 end
 

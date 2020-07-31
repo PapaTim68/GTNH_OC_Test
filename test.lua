0937953eb3a0f7e61252587ff1eab7d5d3d6cd00
PapaTim68 <tim.papenhausen@gmx.de>
2020-07-31 16:54:53 +0200

    Test Inventory Controller

---
+++ test.lua
@@ -0,0 +1,16 @@
+local component = require("component")
+local sides = require("sides")
+
+local inv = component.inventory_controller
+
+items = inv.getAllStacks(sides.east)
+
+for item_n = 0, items.count() do
+    item = items[item_n]
+    print("--ITEM " .. item_n .. " START--")
+    for k,v in pairs(item) do
+        print(k,v)
+    end
+    print("--ITEM " .. i .." END--") 
+end
+

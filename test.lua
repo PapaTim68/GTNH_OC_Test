d62fa2bc3ed0c8a9aed380a22acb0ff96339ac22
PapaTim68 <tim.papenhausen@gmx.de>
2020-07-31 17:51:31 +0200

    transfaare

--- test.lua
+++ test.lua
@@ -1,12 +1,21 @@
 local component = require("component")
 local sides = require("sides")
 
-inv = component.inventory_controller
+inv = component.transposer
+player_side = sides.west
+chest_side = sides.east
 
-items = inv.getAllStacks(sides.east)
+function transfareToChest(SLOT,COUNT)
+    local chest = inv.getAllStacks(chest_side)
+    for slot_n = 1, chest.count() do
+        slot_c = chest[slot_n]
+        if slot_c == nil then
+            inv.transferItem(player_side,chest_side,COUNT,SLOT,slot_n)
 
-for item_n = 1, items.count() do
-    item = items[item_n]
+player = inv.getAllStacks(player_side)
+
+for item_n = 0, player.count()-1 do
+    item = player[item_n]
     if item == nil then
         print("--EMPTY ".. item_n .. " SLOT--")
     else
@@ -14,8 +23,11 @@ for item_n = 1, items.count() do
         for k,v in pairs(item) do
             print(k,v)
         end
-        print("--ITEM " .. item_n .." END--") 
-        io.read()
+        print("--ITEM " .. item_n .." END--")
+        print("Do you want to transfare it? [Y|n]")
+        ans = io.read()
+        if ans=="Y" then
+            transfareToChest(item_n,item["size"])
+
     end
 end
-
